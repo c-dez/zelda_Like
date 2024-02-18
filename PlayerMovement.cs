c3e8bdc64952c5f8eba64b40b7c506f96144678b
c-dez <hdezcruzcarlos@gmail.com>
2024-02-17 19:35:56 -0600

    added simple movement

---
+++ PlayerMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private CharacterController controller;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical);
+
+        if(direction.magnitude >=0.1f)
+        {
+            controller.Move(direction * speed *Time.deltaTime);
+        }
+    }
+
+}
+
